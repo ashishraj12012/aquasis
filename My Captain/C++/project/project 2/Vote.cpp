f0e1a47f885eddbb3963e830b8ce4a4ca0b62181
ashishraj12012 <72972105+ashishraj12012@users.noreply.github.com>
2021-10-28 09:44:30 +0530

    Add files via upload

---
+++ My Captain/C++/project/project 2/Vote.cpp
@@ -0,0 +1,18 @@
+//C++ program where we check if a person is eligible to vote or not
+#include<iostream>
+using namespace std;
+
+int main() {
+
+    int age;
+    cout << "Enter age of a user:";
+    cin>>age;
+
+    if (age >= 18) {
+        cout << "You are eligible for voting";
+    } else {
+        cout << "You are not eligible for voting";
+    }
+
+    return 0;
+}
